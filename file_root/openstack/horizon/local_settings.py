6ff7f49e7ed86211fd5d4e6a9ed96dacdaf08287
Yee-Ting Li <yee379@gmail.com>
2016-03-18 00:09:12 -0700

    force keystonev3

--- file_root/openstack/horizon/local_settings.py
+++ file_root/openstack/horizon/local_settings.py
@@ -11,6 +11,7 @@ TEMPLATE_DEBUG = DEBUG
 # as per http://docs.openstack.org/developer/horizon/topics/deployment.html#secure-site-recommendations
 USE_SSL = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTOCOL', 'https')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 CSRF_COOKIE_HTTPONLY = True
 SESSION_COOKIE_HTTPONLY = True
 CSRF_COOKIE_SECURE = True
@@ -21,6 +22,71 @@ OPENSTACK_SSL_NO_VERIFY = True
 {% endif %}
 
 WEBROOT = '/dashboard/'
+LOGIN_URL='/dashboard/auth/login/'
+LOGOUT_URL='/dashboard/auth/logout/'
+# LOGIN_REDIRECT_URL can be used as an alternative for
+# HORIZON_CONFIG.user_home, if user_home is not set.
+# Do not set it to '/home/', as this will cause circular redirect loop
+LOGIN_REDIRECT_URL='/dashboard'
+
+# Overrides for OpenStack API versions. Use this setting to force the
+# OpenStack dashboard to use a specific API version for a given service API.
+# Versions specified here should be integers or floats, not strings.
+# NOTE: The version should be formatted as it appears in the URL for the
+# service API. For example, The identity service APIs have inconsistent
+# use of the decimal point, so valid options would be 2.0 or 3.
+OPENSTACK_API_VERSIONS = {
+#    "data-processing": 1.1,
+   "identity": 3,
+#    "volume": 2,
+}
+
+# Set this to True if running on multi-domain model. When this is enabled, it
+# will require user to enter the Domain name in addition to username for login.
+#OPENSTACK_KEYSTONE_MULTIDOMAIN_SUPPORT = False
+
+# Overrides the default domain used when running on single-domain model
+# with Keystone V3. All entities will be created in the default domain.
+#OPENSTACK_KEYSTONE_DEFAULT_DOMAIN = 'Default'
+
+# Set Console type:
+# valid options are "AUTO"(default), "VNC", "SPICE", "RDP", "SERIAL" or None
+# Set to None explicitly if you want to deactivate the console.
+#CONSOLE_TYPE = "AUTO"
+
+# Show backdrop element outside the modal, do not close the modal
+# after clicking on backdrop.
+#HORIZON_CONFIG["modal_backdrop"] = "static"
+
+# Specify a regular expression to validate user passwords.
+#HORIZON_CONFIG["password_validator"] = {
+#    "regex": '.*',
+#    "help_text": _("Your password does not meet the requirements."),
+#}
+
+LOCAL_PATH = os.path.dirname(os.path.abspath(__file__))
+
+# Set custom secret key:
+# You can either set it to a specific value or you can let horizon generate a
+# default secret key that is unique on this machine, e.i. regardless of the
+# amount of Python WSGI workers (if used behind Apache+mod_wsgi): However,
+# there may be situations where you would want to set this explicitly, e.g.
+# when multiple dashboard instances are distributed on different machines
+# (usually behind a load-balancer). Either you have to make sure that a session
+# gets all requests routed to the same dashboard instance or you set the same
+# SECRET_KEY for all of them.
+SECRET_KEY='{{ secret_key }}'
+
+# We recommend you use memcached for development; otherwise after every reload
+# of the django development server, you will have to login again. To use
+# memcached set CACHES to something like
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
+
 ALLOWED_HOSTS = ['*']
 HORIZON_CONFIG = {
     'user_home': 'openstack_dashboard.views.get_user_home',
@@ -42,19 +108,16 @@ HORIZON_CONFIG = {
     'password_autocomplete': 'off',
     'disable_password_reveal': False,
 }
-LOCAL_PATH = os.path.dirname(os.path.abspath(__file__))
-SECRET_KEY='{{ secret_key }}'
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
-    }
-}
-LOGIN_URL='/dashboard/auth/login/'
-LOGOUT_URL='/dashboard/auth/logout/'
-LOGIN_REDIRECT_URL='/dashboard'
+
 COMPRESS_OFFLINE = True
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# For multiple regions uncomment this configuration, and add (endpoint, title).
+#AVAILABLE_REGIONS = [
+#    ('http://cluster1.example.com:5000/v2.0', 'cluster1'),
+#    ('http://cluster2.example.com:5000/v2.0', 'cluster2'),
+#]
+
 LAUNCH_INSTANCE_LEGACY_ENABLED = True
 LAUNCH_INSTANCE_NG_ENABLED = False
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
@@ -62,6 +125,28 @@ AUTH_USER_MODEL = 'openstack_auth.User'
 OPENSTACK_HOST = "{{ controller_ip }}"
 OPENSTACK_KEYSTONE_URL = "{{ keystone_url }}"
 OPENSTACK_KEYSTONE_DEFAULT_ROLE = "_member_"
+
+# Enables keystone web single-sign-on if set to True.
+#WEBSSO_ENABLED = False
+# Determines which authentication choice to show as default.
+#WEBSSO_INITIAL_CHOICE = "credentials"
+
+# The list of authentication mechanisms
+# which include keystone federation protocols.
+# Current supported protocol IDs are 'saml2' and 'oidc'
+# which represent SAML 2.0, OpenID Connect respectively.
+# Do not remove the mandatory credentials mechanism.
+#WEBSSO_CHOICES = (
+#    ("credentials", _("Keystone Credentials")),
+#    ("oidc", _("OpenID Connect")),
+#    ("saml2", _("Security Assertion Markup Language")))
+
+# The OPENSTACK_KEYSTONE_BACKEND settings can be used to identify the
+# capabilities of the auth backend for Keystone.
+# If Keystone has been configured to use LDAP as the auth backend then set
+# can_edit_user to False and name to 'ldap'.
+#
+# TODO(tres): Remove these once Keystone has an API to identify auth backend.
 OPENSTACK_KEYSTONE_BACKEND = {
     'name': 'native',
     'can_edit_user': True,
@@ -90,6 +175,27 @@ OPENSTACK_NEUTRON_NETWORK = {
     'supported_provider_types': ['*'],
     'supported_vnic_types': ['*']
 }
+
+# The OPENSTACK_IMAGE_BACKEND settings can be used to customize features
+# in the OpenStack Dashboard related to the Image service, such as the list
+# of supported image formats.
+#OPENSTACK_IMAGE_BACKEND = {
+#    'image_formats': [
+#        ('', _('Select format')),
+#        ('aki', _('AKI - Amazon Kernel Image')),
+#        ('ami', _('AMI - Amazon Machine Image')),
+#        ('ari', _('ARI - Amazon Ramdisk Image')),
+#        ('docker', _('Docker')),
+#        ('iso', _('ISO - Optical Disk Image')),
+#        ('ova', _('OVA - Open Virtual Appliance')),
+#        ('qcow2', _('QCOW2 - QEMU Emulator')),
+#        ('raw', _('Raw')),
+#        ('vdi', _('VDI - Virtual Disk Image')),
+#        ('vhd', ('VHD - Virtual Hard Disk')),
+#        ('vmdk', _('VMDK - Virtual Machine Disk')),
+#    ]
+#}
+
 IMAGE_CUSTOM_PROPERTY_TITLES = {
     "architecture": _("Architecture"),
     "kernel_id": _("Kernel ID"),
